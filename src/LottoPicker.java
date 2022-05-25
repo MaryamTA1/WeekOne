8b19ad237117837d7f4c63ff526b60ba512cafef
maryam adebiyi <maryamadebiyi1@gmail.com>
2022-05-25 16:28:41 -0400

    erased the word Hello

--- src/LottoPicker.java
+++ src/LottoPicker.java
@@ -3,8 +3,9 @@ import java.util.Random;
 public class LottoPicker {
     public static void main(String[] args) {
         System.out.println("Application Starting...");
-
-
+        System.out.println("from Java! Yay we're programming");
 
     }
+
 }
+
